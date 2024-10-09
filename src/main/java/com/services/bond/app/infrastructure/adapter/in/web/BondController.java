84058019e879bf18787203927982ac8dcef0105b
maxizelazny <maximiliano.zelazny@tecval.com.ar>
2024-10-09 13:35:54 -0300

    .

--- src/main/java/com/services/bond/app/infrastructure/adapter/in/web/BondController.java
+++ src/main/java/com/services/bond/app/infrastructure/adapter/in/web/BondController.java
@@ -13,7 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/bono")
+@RequestMapping("/bond")
 public class BondController {
     private static final Logger log = LoggerFactory.getLogger(BondController.class);
     private static final String REGISTERING_BOND = "registering bond...";
