155a6c378afa9b4285ccb416845a52672bb8408c
Julian Lopez <julianmarcelolopez@gmail.com>
2024-10-15 10:37:15 -0300

    adding BondApplicationTests

---
+++ src/test/java/BondApplicationTests.java
@@ -0,0 +1,12 @@
+import com.services.bond.app.BondApplication;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = BondApplication.class)
+class BondApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
